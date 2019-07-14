64a56d51058f831570cc09d17f8fc0b749146abb
AnushaPalla <anushareddypalla9@gmail.com>
2019-07-14 16:33:59 -0500

    Anusha Palla
    Anusha Palla

---
+++ Spark/ICP3/Source/build.sbt
@@ -0,0 +1,11 @@
+name := "sparkicp3"
+
+version := "0.1"
+
+scalaVersion := "2.11.8"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-core
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.3.2"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-sql
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.3.2"
---
+++ Spark/ICP3/Source/src/main/scala/ICP3.scala
@@ -0,0 +1,119 @@
+
+import org.apache.log4j._
+import org.apache.spark._
+import org.apache.spark.sql.SparkSession
+
+object ICP3  {
+
+  def main(args: Array[String]): Unit = {
+
+    System.setProperty("hadoop.home.dir","C:\\winutils" );
+    // Running Spark in local mode, so we  need to set master to local
+    val conf = new SparkConf().setMaster("local[2]").setAppName("my app")
+    // Spark configuration and create Spark context with Spark configuration
+    val sc = new SparkContext(conf)
+    // The entry point into Spark functionality is the SparkSession class
+    // create SparkSession by SparkSession.builder():
+    val spark = SparkSession
+      .builder()
+      .appName("Spark SQL basic example")
+      .config(conf =conf)
+      .getOrCreate()
+
+
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    Logger.getLogger("akka").setLevel(Level.ERROR)
+
+    // For implicit conversions that is for converting RDDs to DataFrames
+    import spark.implicits._
+
+    // Create a DataFrame
+    val df = spark.read
+      .format("csv")
+      .option("header", "true") // reading the headers
+      .option("mode", "DROPMALFORMED")
+      .load("C:\\Users\\Kavya Reddy\\IdeaProjects\\sparkicp3\\survey.csv")
+    df.write.format("csv").option("header","true").save("C:\\Users\\Kavya Reddy\\IdeaProjects\\sparkicp3\\output2")
+
+
+    //Apply Union operation on the dataset and order the output by Country Name alphabetically.
+    val df1 = df.limit(5)//dataframe1
+    val df2 = df.limit(10)//dataframe2
+    println("Dataframe 1")
+    df1.show()
+    println("Dataframe 2")
+    df2.show()
+    val unionDf = df1.union(df2)//union operation
+    println("Union")
+    unionDf.orderBy("Country").show()//order by country
+
+    //// Register the DataFrame as a SQL temporary view
+    df.createOrReplaceTempView("survey")
+
+    // Duplicate
+
+    val DuplicateDF = spark.sql("select COUNT(*),Country,Timestamp from survey GROUP By Timestamp,Country Having COUNT(*) > 1")
+    //println("Check duplicate")
+    //val unique=df.dropDuplicates().show()
+    val unique= df.groupBy("Timestamp","Age","Gender","Country","state","self_employed","family_history","treatment","work_interfere","no_employees","remote_work","tech_company","benefits","care_options").count.filter($"count">1).show()
+    val total=df.count()
+    //DupDF.show(50)
+    println("Number of duplicate records:"+unique)
+    println("Total no of records:"+total)
+
+
+
+    //Use Groupby Query based on treatment.
+
+    val treatmentgroup = spark.sql("select count(Country) from survey GROUP BY treatment ")
+    println("group by treatment")
+    treatmentgroup.show()
+
+
+    //Aggregate Max and Average
+    val MaxDF = spark.sql("select Max(Age) from survey")
+    println("Maximum of age")
+    MaxDF.show()
+
+    val AvgDF = spark.sql("select Avg(Age) from survey")
+    println("Average of age")
+    AvgDF.show()
+
+
+
+    // Join the dataframe using sql
+
+    val df3 = df.limit(50)
+    val df4 = df.limit(80)
+
+    df3.createOrReplaceTempView("left")
+    df4.createOrReplaceTempView("right")
+
+
+    val joinSQl = spark.sql("select left.state,right.Country FROM left,right where left.Age = " +
+      "right.Age")
+    println("left join and right join")
+    joinSQl.show()
+
+    //13th Row from DataFrame
+    val df13th = df.take(13).last
+    println("13th row of dataset")
+    print(df13th)
+
+
+    def parseLine(line: String) =
+    {
+      val fields = line.split(",")
+      val timestamp = fields(0).toString
+      val age = fields(1).toString
+      val noemply = fields(9).toString
+      (timestamp,age, noemply)
+    }
+
+    val lines = sc.textFile("C:\\Users\\Kavya Reddy\\IdeaProjects\\sparkicp3\\survey.csv")
+    val rdd = lines.map(parseLine).toDF()
+    println("")
+    println("After parse Line")
+    rdd.show()
+  }
+}
